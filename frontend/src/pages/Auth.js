77fe54d840118f27d587085f1361df924c9b0505
Bo Yang <boyang@ebay.com>
2022-03-13 18:25:49 +0800

    support frontend auth

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,9 +1,114 @@
 import React, { Component } from "react";
+import "./Auth.css";
+import AuthContext from "../context/auth-context";
 
 class AuthPage extends Component {
-    render() {
-        return (<h1>The Auth Page</h1>);
+  state = {
+    isLogin: true,
+  };
+
+  static contextType = AuthContext;
+
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  switchModeHandler = () => {
+    this.setState((prevState) => {
+      return { isLogin: !prevState.isLogin };
+    });
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
     }
+
+    let requestBody = {
+      query: `
+        query {
+            login(loginInput: {email: "${email}", password: "${password}"}) {
+                userId,
+                token,
+                tokenExpiration
+            }
+        }
+        `,
+    };
+
+    if (!this.state.isLogin) {
+      requestBody = {
+        query: `
+                mutation {
+                    createUser(userInput: {
+                        email: "${email}",
+                        password: "${password}"
+                    }) {
+                        id
+                        email
+                    }
+                }
+              `,
+      };
+    }
+
+    console.log(email, password);
+    // https://www.baeldung.com/spring-cors
+    fetch("http://localhost:8080/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log(resData);
+        if (resData.data.login.token) {
+          this.context.login(
+            resData.data.login.token,
+            resData.data.login.userId,
+            resData.data.login.tokenExpiration
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // TODO
+  };
+
+  render() {
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" ref={this.emailEl} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={this.switchModeHandler}>
+            Switch to {this.state.isLogin ? "Signup" : "Login"}
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
-export default AuthPage
+export default AuthPage;
